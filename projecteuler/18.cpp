d29c24c783106b4c3e15e7ee8042a6a2b0d6a50d
andytsen <andydomokun@gmail.com>
2016-06-21 21:01:10 +0800

    ProjectEuler

---
+++ projecteuler/18.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() 
+{
+    string line;
+    int most = 0;
+    int arr[15];
+    while(getline(cin, line))     
+    {
+        
+    }
+    return 0;
+}
