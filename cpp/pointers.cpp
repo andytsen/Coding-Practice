cdca165e5ea5cafccac59f7285dccb1b95aabe2f
teeseng <andydomokun@gmail.com>
2016-09-19 11:03:02 -0500

    pointers

--- cpp/pointers.cpp
+++ cpp/pointers.cpp
@@ -4,18 +4,6 @@ int* g(int*&, int*, int&, int);
 
 void f()
 {
-    int a = 4;
-    int& j = a;
-    a++;
-    std::cout << a << " " << j << std::endl;
-
-    int* b = &a; // x
-    std::cout << *b << " " << ++(*b) << " " <<  a << " " << std::endl; 
-
-    int j = 4;
-
-    
-     
 }
 
 /*
@@ -27,8 +15,11 @@ int* g(int*& a, int* b, int& c, int d)
 
 int main()
 {
+    int i = 9;
+    int& a = i; // int& on the LEFT expects a LEFT value 
+    int* b = &a; 
 
-    f();
     
+     
     return 0;
 }
