98f3779ec135d357cbd962a97a096bc9a3eb4ea5
teeseng <andydomokun@gmail.com>
2017-07-03 21:09:52 -0500

    code for the past week

---
+++ data_structures/pq.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+class Node {
+    private:
+        int data;
+        Node* left;
+        Node* right;
+    public:
+        Node(int d) : data(d) {}
+};
+
+class PriorityQueue {
+    private:
+        Node* head;
+    public:
+        void insert(int n) {
+
+        }
+};
+
+int main() {
+
+}
