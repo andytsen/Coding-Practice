aef207589a2d9baa48c4c11a54a4cd7c6c22afb7
teeseng <andydomokun@gmail.com>
2017-08-14 14:42:09 -0700

    some updates

---
+++ leetcode/medium/waterjugs.cpp
@@ -0,0 +1,17 @@
+// operations allowed
+//  fill any jug up with water
+//  empty any of the jugs
+//  pour into other jug until recepient is full
+//  pour until giver is empty
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+}
+
+bool canMeasureWater(int x,int y,int z) {
+
+}
