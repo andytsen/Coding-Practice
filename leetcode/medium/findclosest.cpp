8710ee2de76fe19f70201672559a604d0d7b8b95
teeseng <andydomokun@gmail.com>
2017-08-17 16:39:50 -0700

    naive fce

--- leetcode/medium/findclosest.cpp
+++ leetcode/medium/findclosest.cpp
@@ -3,44 +3,38 @@
 
 using namespace std;
 
+// O(n)
 vector<int> findClosestElement(vector<int>& nums, int k, int x) {
-    vector<int> ans = vector<int>();
-    int r = nums.size() - 1;
-    int l = 0;
-    int closest = 0;
-    while(r - l > 1) {
-        int diffleft = abs(nums[l] - x);
-        int diffright = abs(nums[r] - x);
-        int mid = (r - l)/2;
-        if(diffright < diffleft) {
-            l += mid;
-        } else if(diffleft < diffright) {
-            r -= mid;
-        } else {
-            r -= mid;
+    vector<int> ans = vector<int>();  
+    int i;
+    for(i = 0; i + 1 < nums.size(); i++) {
+        if(nums[i] < x && nums[i+1] > x) {
+            break;
         }
+        if(nums[i] == x)
+            break;
     }
-
+    if(i == nums.size() - 1)
+        i = x < nums[0] ? 0 : nums.size() - 1;
+    int l = i;
+    int r = i+1;
     while(k > 0) {
+        k--;
         int diffleft = l >= 0 ? abs(nums[l] - x) : INT_MAX;
         int diffright = r < nums.size() ? abs(nums[r] - x) : INT_MAX;
-        if(diffleft > diffright) {
+        if(diffleft < diffright) {
+            vector<int> :: iterator it = ans.begin();
+            ans.insert(it,nums[l]);
+            l--;
+        } else if(diffright < diffleft) {
             ans.push_back(nums[r]);
             r++;
         } else {
-            vector<int>::iterator it = ans.begin();
+            vector<int> :: iterator it = ans.begin();
             ans.insert(it,nums[l]);
             l--;
         }
-        k--;
     }
-
-    /* Debug */
-    for(int i : ans)
-        cout << i << " ";
-    cout << endl;
-    cout << "x = " << x << endl;
-
     return ans;
 }
 
