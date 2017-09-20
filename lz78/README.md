9c71dc83d51e89d8a5f076705a2b8d196790a9ef
teeseng <andydomokun@gmail.com>
2017-09-20 18:23:36 -0500

    long needed sync

---
+++ lz78/README.md
@@ -0,0 +1,2 @@
+Sources:
+http://math.mit.edu/~goemans/18310S15/lempel-ziv-notes.pdf
---
+++ lz78/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+void encode();
+
+int main() {
+    char current_char;
+    while(cin >> current_char) {
+
+    }
+}
