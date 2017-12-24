94182fcb01dac0083df0c8e97cc40c0fb3d2b028
teeseng <andydomokun@gmail.com>
2017-12-23 18:35:32 -0600

    mergesort for int list

--- algorithms/mergesort/mergesort.cpp
+++ algorithms/mergesort/mergesort.cpp
@@ -11,13 +11,13 @@ void merge(vector<int>&, int, int, int);
 void merge_sort(vector<int>& a, int low, int high) {
     if(high - low == 1) {
         if(a[low] > a[high]) {
-            int temp = a[high];
-            a[high] = a[low];
-            a[low] = temp;
+            int tmp = a[low];
+            a[low] = a[high];
+            a[high] = tmp; 
         }
     }
-    else if(high != low) {
-        int mid = (low + high) >> 1;
+    else if(high > low) {
+        int mid = (high >> 1) + low;
         merge_sort(a, low, mid - 1);
         merge_sort(a, mid, high);
         merge(a, low, mid, high);
@@ -31,11 +31,19 @@ void swap(int& a, int& b) {
 }
 
 void merge(vector<int>& a, int low, int mid, int high) {
+    if(low == high) return;
     int i = low;
     int j = mid;
-    vector<int> ss = vector<int>(high - low);
-    while(i < mid - 1 || j < high) {
-        if(a[i] < a[j]) {
+    vector<int> ss;
+    while(i < mid || j < high) {
+        if(i >= mid) {
+            ss.push_back(a[j]);
+            ++j;
+        }
+        else if(j >= high) {
+            ss.push_back(a[i]);
+            ++i;
+        } else if(a[i] < a[j]) {
             ss.push_back(a[i]);
             ++i;
         } else {
@@ -43,16 +51,27 @@ void merge(vector<int>& a, int low, int mid, int high) {
             ++j;
         }
     }
-    for(int i = low; i < high; ++i) {
-        a[i] = ss[i - low];
+    for(int k = low; k < high; ++k) {
+        a[k] = ss[k - low];
     }
 }
 
 int main() {
+    cout << "TEST 1: " << endl;
     vector<int> t1 = {1, 5, 3, 9, 2, -1};
-    merge(t1, 0, 3, t1.size());
+    merge_sort(t1, 0, t1.size() - 1);
+    for(int i : t1) {
+        cout << i << " ";
+    }
+    cout << endl;
 
-    for(int c : t1) { cout << c << " "; }
+    vector<int> t2 = {9, 4, -1};
+    merge_sort(t2, 0, t2.size() - 1);
+    cout << endl;
+    cout << "TEST 2: " << endl;
+    for(int i : t2) {
+        cout << i << " ";
+    }
     cout << endl;
     return 0;
 }
