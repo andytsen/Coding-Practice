4902d5289f773293bb6cfd136d5272ab9d62e65c
teeseng <andydomokun@gmail.com>
2017-12-28 16:27:08 -0800

    more leetcode

--- algorithms/mergesort/mergesort.cpp
+++ algorithms/mergesort/mergesort.cpp
@@ -1,40 +1,20 @@
-// Used to sort a vector or array
+// ------------------
+// Mergesort algorithm for all vectors
+// by Andy Tseng
+//
+// ------------------
+
 #include <vector>
 #include <iostream>
 
 using namespace std;
 
-void merge_sort(vector<int>&, int, int);
-void swap(int&, int&);
-void merge(vector<int>&, int, int, int);
-
-void merge_sort(vector<int>& a, int low, int high) {
-    if(high - low == 1) {
-        if(a[low] > a[high]) {
-            int tmp = a[low];
-            a[low] = a[high];
-            a[high] = tmp; 
-        }
-    }
-    else if(high > low) {
-        int mid = (high >> 1) + low;
-        merge_sort(a, low, mid - 1);
-        merge_sort(a, mid, high);
-        merge(a, low, mid, high);
-    }
-}
-
-void swap(int& a, int& b) {
-    int temp = a;
-    a = b;
-    b = temp;
-}
-
-void merge(vector<int>& a, int low, int mid, int high) {
+template<typename T>
+void merge(vector<T>& a, int low, int mid, int high) {
     if(low == high) return;
     int i = low;
     int j = mid;
-    vector<int> ss;
+    vector<T> ss;
     while(i < mid || j < high) {
         if(i >= mid) {
             ss.push_back(a[j]);
@@ -56,8 +36,30 @@ void merge(vector<int>& a, int low, int mid, int high) {
     }
 }
 
+template<typename T>
+void merge_sort(vector<T>& a, int low, int high) {
+    if(high - low == 1) {
+        if(a[low] > a[high]) {
+            T tmp = a[low];
+            a[low] = a[high];
+            a[high] = tmp; 
+        }
+    }
+    else if(high > low) {
+        int mid = (high >> 1) + low;
+        merge_sort(a, low, mid - 1);
+        merge_sort(a, mid, high);
+        merge(a, low, mid, high);
+    }
+}
+
+template<typename T>
+void merge_sort(vector<T>& a) {
+    merge_sort(a, 0, a.size() - 1);
+}
+
 int main() {
-    cout << "TEST 1: " << endl;
+    cout << "--- TEST 1 ---" << endl;
     vector<int> t1 = {1, 5, 3, 9, 2, -1};
     merge_sort(t1, 0, t1.size() - 1);
     for(int i : t1) {
@@ -67,11 +69,19 @@ int main() {
 
     vector<int> t2 = {9, 4, -1};
     merge_sort(t2, 0, t2.size() - 1);
-    cout << endl;
-    cout << "TEST 2: " << endl;
+    cout << "--- TEST 2 ---" << endl;
     for(int i : t2) {
         cout << i << " ";
     }
     cout << endl;
+
+    cout << "--- TEST 3 ---" << endl;
+    vector<double> t3 = {4.1, 9.0001, 5.000023, 3.14159, 1.09, 0.993};
+    merge_sort(t3);
+    for(double db : t3) {
+        cout << db << " ";
+    }
+    cout << endl;
+
     return 0;
 }
--- leetcode/medium/combination_sum1.cpp
+++ leetcode/medium/combination_sum1.cpp
@@ -3,32 +3,42 @@
 
 using namespace std;
 
-int combination(vector<int>& nums, int tgt);
+void recur_combination(vector<vector<int>>& res, vector<int>& cur, vector<int>& nums, int tgt, int start);
+vector<vector<int>> combination(vector<int>& nums, int tgt);
 
 int main() {
-    vector<int> a;
-    a.push_back(2);
-    a.push_back(2);
-    a.push_back(3);
-    a.push_back(4);
-    cout << combination(a, 7) << endl;
+    vector<int> a = {2, 3, 6, 7};
+    sort(a.begin(), a.end());
+    vector<vector<int>> res = combination(a, 7);
+    cout << endl;
+    for(vector<int> v : res) {
+        for(int i : v) {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
     return 0;
 }
 
-int combination(vector<int>& nums, int tgt) {
-    vector<int> dp(tgt + 1, 0);
-    dp[0] = 1;
-    for(int i = 1; i <= tgt; i++) {
-        for(int n: nums) {
-            if(n == i) {
-                dp[i] += dp[i-n];
-            }
+vector<vector<int>> combination(vector<int>& nums, int tgt) {
+    vector<vector<int>> res; 
+    vector<int> cur;
+    recur_combination(res, cur, nums, tgt, 0);
+    return res;
+}
+
+void recur_combination(vector<vector<int>>& res, vector<int>& cur, vector<int>& nums, int tgt, int start) {
+    if(tgt < 0) {
+        return;
+    }
+    else if(tgt == 0) {
+        res.push_back(cur);
+    } else {
+        for(int i = start; i < nums.size(); ++i) {
+            cur.push_back(nums[i]);
+            recur_combination(res, cur, nums, tgt - nums[i], i);
+            cur.erase(cur.end() - 1);
         }
     }
-
-    for(int i = 0; i < tgt + 1; i++) {
-        cout << dp[i] << " ";
-    }
-    cout << endl;
-    return dp[tgt];
 }
