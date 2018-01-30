026ddad45178fc9af749c7ba6f040591759e23d1
teeseng <andydomokun@gmail.com>
2018-01-30 14:39:25 -0600

    update on leetcode

---
+++ junk/def.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#define ANDY() cout << "andy" << endl;
+
+#define MACRO_1 7
+
+using namespace std;
+int main() {
+    ANDY();
+    cout << MACRO_1 << endl;
+    return 0;
+}
+
+
+
+
---
+++ system/README.md
@@ -0,0 +1,3 @@
+## toybox/system
+
+Mental model on some operating system stuff that I want to refresh over. 
