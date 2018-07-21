0d06320f20715126b460f8149ae0acfc42617495
teeseng <andydomokun@gmail.com>
2018-07-21 15:57:49 -0500

    huh.

--- prawtest/foopraw.py
+++
@@ -1,9 +0,0 @@
-#/usr/local/lib
-import praw
-
-reddit = praw.Reddit('bot1',user_agent='some scraper')
-
-for submission in reddit.subreddit('leagueoflegends').hot(limit=11):
-    if(submission.stickied == False):
-        print(submission.title)
-        print("\n")
