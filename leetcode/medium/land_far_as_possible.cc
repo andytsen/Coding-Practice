861b8ff66264a1074144b8d724b1baee2984f6d2
teeseng <andydomokun@gmail.com>
2019-08-19 07:18:33 -0500

    for today

---
+++ leetcode/medium/land_far_as_possible.cc
@@ -0,0 +1,23 @@
+
+#include <vector>
+
+using namespace std;
+
+int farest_land(vector<vector<int>>& grid)
+{
+    vector<vector<int>> closest_land_dist;
+    for(int r = 0; r < grid.size(); ++r) 
+    {
+        for(int c = 0; c < grid[0].size(); ++c)
+        {
+            
+        }
+    }
+
+}
+
+
+int main() 
+{
+    return 0;
+}
